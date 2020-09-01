3fa41145bf2d4eac3fa1da94e6865da2c6a27366
razonyang <razonyang@gmail.com>
2020-09-01 12:15:10 +0800

    Update

--- cmd/root.go
+++ cmd/root.go
@@ -10,13 +10,22 @@ import (
 )
 
 var (
+	cfg string
 	app = &cli.App{
 		EnableBashCompletion: true,
 		Version:              core.Version,
 		Name:                 "go-auth0-web-app",
-		Usage:                "Go Web Application",
+		Usage:                "Go Auth0 Web Application",
+		Flags: []cli.Flag{
+			&cli.StringFlag{
+				Name:        "config",
+				Aliases:     []string{"c"},
+				Value:       ".env",
+				Destination: &cfg,
+			},
+		},
 		Before: func(c *cli.Context) error {
-			if err := godotenv.Load(); err != nil {
+			if err := godotenv.Load(cfg); err != nil {
 				return err
 			}
 
